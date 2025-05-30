62fb0a1608e4f3cbf1fc17ff7cfae0a1769c2b28
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-30 21:05:54 +0300

    book created /Composition + Interface

--- src/model/Book.java
+++ src/model/Book.java
@@ -1,4 +1,28 @@
 package model;
 
+//Composition + Interface ekle
+
+
 public class Book {
+    private String id;
+    private String title;
+    private Author author;
+    private Category category;
+    private boolean isBorrowed = false;
+
+    public Book(String id, String title, Author author, Category category) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+    }
+
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public Author getAuthor() { return author; }
+    public Category getCategory() { return category; }
+    public boolean isBorrowed() { return isBorrowed; }
+
+    public void borrow() { this.isBorrowed = true; }
+    public void returnBook() { this.isBorrowed = false; }
 }
