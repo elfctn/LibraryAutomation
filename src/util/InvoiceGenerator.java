d951ff644cdf5849467ca6a6db986ae741df2ebc
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-30 21:10:08 +0300

    invoice generator class created

--- src/util/InvoiceGenerator.java
+++ src/util/InvoiceGenerator.java
@@ -1,4 +1,13 @@
 package util;
+//Basit fatura üretiyor
+
+import model.*;
+
+import java.util.UUID;
 
 public class InvoiceGenerator {
+    public static Invoice generateInvoice(User user, Book book, double amount) {
+        String id = UUID.randomUUID().toString();
+        return new Invoice(id, user, book, amount);
+    }
 }
