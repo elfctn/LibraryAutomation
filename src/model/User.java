17962b0e9171f78c09647924203dbb1ba8965f4d
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-30 21:04:13 +0300

    user class created /inheritance

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,24 @@
 package model;
 
-public class User {
+
+//PERSON'DAN KALITIM
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class User extends Person {
+    private Set<Book> borrowedBooks = new HashSet<>();
+
+    public User(String id, String name) {
+        super(id, name);
+    }
+
+    public Set<Book> getBorrowedBooks() {
+        return borrowedBooks;
+    }
+
+    public boolean canBorrow() {
+        return borrowedBooks.size() < 5;
+    }
 }
