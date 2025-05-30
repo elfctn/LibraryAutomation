6efba0d50c30d7a77c90e452a34bba3f8462aab8
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-30 21:03:12 +0300

    person abstract class created

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,4 +1,17 @@
 package model;
 
-public class Person {
+//ABSTRACT SINIF
+
+public abstract class Person {
+    protected String id;
+    protected String name;
+
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
