1a3bbcbc823ded3b733e43de391b1bb319caf88d
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-30 21:07:53 +0300

    invoice class created

--- src/model/Invoice.java
+++ src/model/Invoice.java
@@ -1,4 +1,29 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Invoice {
+    private String id;
+    private User user;
+    private Book book;
+    private double amount;
+    private LocalDate date;
+    private boolean isRefunded;
+
+    public Invoice(String id, User user, Book book, double amount) {
+        this.id = id;
+        this.user = user;
+        this.book = book;
+        this.amount = amount;
+        this.date = LocalDate.now();
+        this.isRefunded = false;
+    }
+
+    public String getId() { return id; }
+    public User getUser() { return user; }
+    public Book getBook() { return book; }
+    public double getAmount() { return amount; }
+    public LocalDate getDate() { return date; }
+    public boolean isRefunded() { return isRefunded; }
+    public void refund() { this.isRefunded = true; }
 }
