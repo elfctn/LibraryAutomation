77b4562ad0ec4daca4ceea732dc469a3aa20eeff
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-30 21:09:13 +0300

    data store class created

--- src/data/DataStore.java
+++ src/data/DataStore.java
@@ -1,4 +1,22 @@
 package data;
 
+//Kitap, kullanıcı vb. verileri saklayacak
+
+import model.*;
+
+import java.util.*;
+
+
 public class DataStore {
+    private Map<String, Book> books = new HashMap<>();
+    private Map<String, User> users = new HashMap<>();
+    private Map<String, Author> authors = new HashMap<>();
+    private Map<String, Category> categories = new HashMap<>();
+    private List<Invoice> invoices = new ArrayList<>();
+
+    public Map<String, Book> getBooks() { return books; }
+    public Map<String, User> getUsers() { return users; }
+    public Map<String, Author> getAuthors() { return authors; }
+    public Map<String, Category> getCategories() { return categories; }
+    public List<Invoice> getInvoices() { return invoices; }
 }
