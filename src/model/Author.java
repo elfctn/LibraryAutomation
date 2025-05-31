5ab6faf1c158714ffbc9b50087c4e05738f2da6c
Elif Cetin <elifcetin.dev@gmail.com>
2025-05-31 20:39:27 +0300

    project is running- will continue to be developed

--- src/model/Author.java
+++ src/model/Author.java
@@ -1,15 +1,28 @@
 package model;
 
+import java.util.UUID;
+
 public class Author {
     private String id;
     private String name;
 
-    public Author(String id) {
+    public Author(String id, String name) {
         this.id = id;
+        this.name = name;
+    }
+
+    // Tek parametreli constructor - sadece isim verildiğinde id otomatik atanır
+    public Author(String name) {
+        this.id = UUID.randomUUID().toString();
         this.name = name;
     }
 
     public String getId() { return id; }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    @Override
+    public String toString() {
+        return name + " (ID: " + id + ")";
+    }
 }
--- src/model/Category.java
+++ src/model/Category.java
@@ -9,4 +9,9 @@ public class Category {
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
